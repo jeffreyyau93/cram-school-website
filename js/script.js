39430ad5800749d0b3d34856d20c25534310bd88
Jeffrey Yau <jeffreyyau@outlook.com>
2019-09-27 23:55:55 +0800

    chore: Add daily code update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-/* $("#about").load("about.html", function(responseTxt, statusTxt, xhr) {
+$("#about").load("about.html", function(responseTxt, statusTxt, xhr) {
     if (statusTxt == "success") {
         alert("It works");
     } else if (statusTxt == "error") {
         alert("Error:" + xhr.statustext);
     }
-}); */
-var x = location.href;
+});
+/* var x = location.href;
 
 $("#btn-about").on("click", function(e) {
     e.preventDefault();
     var page = $(this).attr("href");
     $("#btn-about").load(page);
-});
+}); */
